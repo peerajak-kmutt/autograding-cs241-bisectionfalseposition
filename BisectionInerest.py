c56e2293168f6e18d58076c88a3f1ef6a7c33238
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-08-26 09:41:53 +0700

    Update and rename hello.py to BisectionInerest.py

---
+++ BisectionInerest.py
@@ -0,0 +1,10 @@
+import numpy as np
+def BisectionInterst():    
+    #original_eq =  lambda x:  #TODO
+    #is_same_sign = lambda a,b : #TODO
+    xa = 0.0000001
+    xb = 100.0
+    xc = 0 #Ans of the interest rate
+    epsilon = 1e-6
+    #TODO implement Bisection/or False Position here to solve the Interest rate problem
+    return #TODO which variable should you return?
